03febcf82e9d9b30798d9faf04510cf6d04c90b4
Erbao Huang <erbao_huang@apple.com>
2022-02-17 00:09:07 -0800

    UsingARenderPipelineToRenderPrimitives

---
+++ UsingARenderPipelineToRenderPrimitives/Renderer/AAPLRenderer.m
@@ -0,0 +1,141 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Implementation of a platform independent renderer class, which performs Metal setup and per frame rendering
+*/
+
+@import simd;
+@import MetalKit;
+
+#import "AAPLRenderer.h"
+
+// Header shared between C code here, which executes Metal API commands, and .metal files, which
+// uses these types as inputs to the shaders.
+#import "AAPLShaderTypes.h"
+
+// Main class performing the rendering
+@implementation AAPLRenderer
+{
+    id<MTLDevice> _device;
+
+    // The render pipeline generated from the vertex and fragment shaders in the .metal shader file.
+    id<MTLRenderPipelineState> _pipelineState;
+
+    // The command queue used to pass commands to the device.
+    id<MTLCommandQueue> _commandQueue;
+
+    // The current size of the view, used as an input to the vertex shader.
+    vector_uint2 _viewportSize;
+}
+
+- (nonnull instancetype)initWithMetalKitView:(nonnull MTKView *)mtkView
+{
+    self = [super init];
+    if(self)
+    {
+        NSError *error;
+        _device = mtkView.device;
+
+        // Load all the shader files with a .metal file extension in the project.
+        id<MTLLibrary> defaultLibrary = [_device newDefaultLibrary];
+
+        id<MTLFunction> vertexFunction = [defaultLibrary newFunctionWithName:@"vertexShader"];
+        id<MTLFunction> fragmentFunction = [defaultLibrary newFunctionWithName:@"fragmentShader"];
+
+        // Configure a pipeline descriptor that is used to create a pipeline state.
+        MTLRenderPipelineDescriptor *pipelineStateDescriptor = [[MTLRenderPipelineDescriptor alloc] init];
+        pipelineStateDescriptor.label = @"Simple Pipeline";
+        pipelineStateDescriptor.vertexFunction = vertexFunction;
+        pipelineStateDescriptor.fragmentFunction = fragmentFunction;
+        // specifies the number of bytes per pixel, the number of channels
+        // of data stored in a pixel, and the bit layout of those channels
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = mtkView.colorPixelFormat;
+        
+        // PSO is created from MTLRenderPipelineDescriptor on device
+        _pipelineState = [_device newRenderPipelineStateWithDescriptor:pipelineStateDescriptor
+                                                                 error:&error];
+                
+        // Pipeline State creation could fail if the pipeline descriptor isn't set up properly.
+        //  If the Metal API validation is enabled, you can find out more information about what went wrong.
+        NSAssert(_pipelineState, @"Failed to create pipeline state: %@", error);
+
+        // Create the command queue
+        _commandQueue = [_device newCommandQueue];
+    }
+
+    return self;
+}
+
+/// Called whenever view changes orientation or is resized
+- (void)mtkView:(nonnull MTKView *)view drawableSizeWillChange:(CGSize)size
+{
+    NSLog(@"drawableSizeWillChange: %f, %f\n", size.height, size.width);
+    // Save the size of the drawable to pass to the vertex shader.
+    _viewportSize.x = size.width;
+    _viewportSize.y = size.height;
+}
+
+/// Called whenever the view needs to render a frame.
+- (void)drawInMTKView:(nonnull MTKView *)view
+{
+    NSLog(@"drawInMTKView\n");
+    static const AAPLVertex triangleVertices[] =
+    {
+        // 2D positions,    RGBA colors
+        { {  250,  -250 }, { 1, 0, 0, 1 } },
+        { { -250,  -250 }, { 0, 1, 0, 1 } },
+        { {    0,   250 }, { 0, 0, 1, 1 } },
+    };
+
+    // Create a new command buffer for each render pass to the current drawable.
+    id<MTLCommandBuffer> commandBuffer = [_commandQueue commandBuffer];
+    commandBuffer.label = @"MyCommand";
+
+    // Obtain a renderPassDescriptor generated from the view's drawable textures.
+    MTLRenderPassDescriptor *renderPassDescriptor = view.currentRenderPassDescriptor;
+
+    if(renderPassDescriptor != nil)
+    {
+        // Create a render command encoder.
+        id<MTLRenderCommandEncoder> renderEncoder =
+        [commandBuffer renderCommandEncoderWithDescriptor:renderPassDescriptor];
+        renderEncoder.label = @"MyRenderEncoder";
+
+        // Set the region of the drawable to draw into.
+        // created a new viewport within which to draw
+        [renderEncoder setViewport:(MTLViewport){0.0, 0.0, _viewportSize.x, _viewportSize.y, 0.0, 1.0 }];
+        
+        [renderEncoder setRenderPipelineState:_pipelineState];
+
+        // Use setVertexBytes to pass input data to vertex shader
+        // Often, you use buffers (MTLBuffer) to pass data to shaders.
+        // However, when you need to pass only a small amount of data
+        // to the vertex function, as is the case here, copy the data
+        // directly into the command buffer.
+        [renderEncoder setVertexBytes:triangleVertices
+                               length:sizeof(triangleVertices)
+                              atIndex:AAPLVertexInputIndexVertices];
+        
+        [renderEncoder setVertexBytes:&_viewportSize
+                               length:sizeof(_viewportSize)
+                              atIndex:AAPLVertexInputIndexViewportSize];
+
+        // Vertex stage for triangle
+        // the vertex function is called with values of 0, 1, and 2
+        // for the vertexID argument
+        [renderEncoder drawPrimitives:MTLPrimitiveTypeTriangle
+                          vertexStart:0
+                          vertexCount:3];
+
+        [renderEncoder endEncoding];
+
+        // Schedule a present once the framebuffer is complete using the current drawable.
+        [commandBuffer presentDrawable:view.currentDrawable];
+    }
+
+    // Finalize rendering here & push the command buffer to the GPU.
+    [commandBuffer commit];
+}
+
+@end
